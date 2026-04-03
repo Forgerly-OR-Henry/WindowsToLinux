c87c160e96bf28d064562c2c7882be5d810f317c
Forgerly.OR.Henry <2553389978@qq.com>
2026-04-03 14:23:21 +0800

    2026-04-02:14.46

---
+++ src/main/java/gold/debug/wintolin/tools/language/TJava.java
@@ -0,0 +1,25 @@
+package gold.debug.wintolin.tools.language;
+
+import gold.debug.wintolin.attribute.language.AJava;
+
+public class TJava {
+    // Windows环境检测
+    public AJava windowsPathCheck() {
+        return null;
+    }
+
+    // Windows环境部署
+    // 暂不实现
+    public void windowsPathDeploy() {
+    }
+
+    // Linux环境检测
+    // 暂不实现
+    public AJava linuxPathCheck() {
+        return null;
+    }
+
+    // Linux环境部署
+    public void linuxPathDeploy() {
+    }
+}
